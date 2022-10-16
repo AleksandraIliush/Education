a49379d298fd987e0aebb03538aa7c78ea8d55e2
AleksandraIliush <115930986+AleksandraIliush@users.noreply.github.com>
2022-10-16 16:13:25 +0300

    Add files via upload

---
+++ HW 1_10_2.py
@@ -0,0 +1,11 @@
+a, b, c, d, e = int(input()), int(input()),int(input()),int(input()),int(input())
+minValue=a
+if b < minValue:
+    miValue=b
+if c < minValue:
+    minValue=c
+if d < minValue:
+    minValue = d
+if e < minValue:
+    minValue = e
+print(minValue)
