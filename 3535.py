0fb69ac9c37dc0e2cec8e4e18e11fa454ff05fff
AleksandraIliush <115930986+AleksandraIliush@users.noreply.github.com>
2022-10-24 20:24:40 +0300

    Add files via upload

---
+++ 3535.py
@@ -0,0 +1,11 @@
+n = int(input())
+str1='   _~_    '
+str2='  (o o)   '
+str3=' /  V  \  '
+str4='/(  _  )\\ '
+str5='  ^^ ^^   '
+print(str1*n)
+print(str2*n)
+print(str3*n)
+print(str4*n)
+print(str5*n)
